16201ab6a1db52dc321493f070d2fbefd6b3b0b7
ryzecous <r3j9c89@gmail.com>
2021-03-23 19:27:16 -0400

    updated index.js to move database at top of file to help with async problem

--- public/index.js
+++ public/index.js
@@ -1,5 +1,80 @@
 let transactions = [];
 let myChart;
+let db;
+
+// use indexedDB to create a client side database to be used in offline mode
+if (!window.indexedDB) {
+  console.log("Your browser doesn't support a stable version of IndexedDB. Offline Mode will not be available. If you would like to use this application offline please use a browser that supports IndexedDB https://caniuse.com/indexeddb");
+}
+
+// Open a Database
+const request = window.indexedDB.open('offlineBudgetDB', 1)
+
+request.onerror = e => console.log(e.target.errorCode)
+
+request.onupgradeneeded = function(evt) {
+  const db = evt.target.result
+  db.createObjectStore("offlineBudget", { autoIncrement: true })
+}
+
+request.onsuccess = e => {
+  console.log(`Successfully opened database ${e.target.name}`)
+  db = e.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+}
+
+function saveRecord(bankTransaction) {
+  console.log(bankTransaction)
+  // create a transaction on the offlineBudget db with readwrite access
+  const transaction = db.transaction(["offlineBudget"], "readwrite");
+
+  // access your offlineBudget object store
+  const store = transaction.objectStore("offlineBudget");
+
+  // add record to your store with add method.
+  store.add(bankTransaction);
+}
+
+function checkDatabase() {
+  // open a transaction on your offlineBudget db
+  const transaction = db.transaction(["offlineBudget"], "readwrite");
+  // access your OfflineBudget object store
+  const store = transaction.objectStore("offlineBudget");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        // if successful, open a transaction on your OfflineBudget db
+        const transaction = db.transaction(["offlineBudget"], "readwrite");
+
+        // access your OfflineBudget object store
+        const store = transaction.objectStore("OfflineBudget");
+
+        // clear all items in your store
+        store.clear();
+      });
+    }
+  };
+}
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
+
+console.log ("nick")
 
 fetch("/api/transaction")
   .then(response => {
@@ -93,7 +168,7 @@ function sendTransaction(isAdding) {
   }
 
   // create record
-  let transaction = {
+  const transaction = {
     name: nameEl.value,
     value: amountEl.value,
     date: new Date().toISOString()
@@ -136,6 +211,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
+
     saveRecord(transaction);
 
     // clear form
